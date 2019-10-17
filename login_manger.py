bd43ac0c95fef114df246afe997cf7061a6e8e72
wangkunquan <wangkunquan@xiaozhu.com>
2019-10-17 21:58:23 +0800

    添加基本配置文件

---
+++ login_manger.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019-10-17 21:53
+# @Author  : wangkunquan@xiaozhu.com
+# @Site    : 
+# @File    : login_manger.py
+# @Software: PyCharm
---
+++ run.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019-10-17 21:50
+# @Author  : AprilWind Eric
+# @Site    : 
+# @File    : run.py.py
+# @Software: PyCharm
+
+
+def run():
+    print("start")
+
+
+if __name__ == '__main__':
+    run()
