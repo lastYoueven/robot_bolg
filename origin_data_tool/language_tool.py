d756fd3b27f46ad0d5484245af7cd145f3a8df24
lastWakeLess <m15182999762@163.com>
2020-03-03 22:56:55 +0800

    添加nltk TODO

---
+++ origin_data_tool/language_tool.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class LanguageBaseTool:
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def test(self):
+        """
+        测试
+        """
+        pass
+
+
+def test():
+    """
+    测试
+    """
+    pass
+
+
+if __name__ == '__main__':
+    test()
